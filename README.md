14d1c03e5cd369ddb365bc9ebaa3d76d8f716901
Zhangyao719 <1258858403@qq.com>
2023-04-12 20:38:03 +0800

    Config splitChunks

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # 从零配置 webpack4 + react 脚手架
 
 https://github.com/vortesnail/blog/issues/4
+
+## 配置步骤
+
+1. 基础配置
+    - 安装 webpack、webpack-cli
+    - webpack-merge 区分环境
+2. babel 配置
+   解析 jsx、es6
+3. html 模板
+4. 代码分片
+    - optimization.splitChunks
+      多 entry 入口 配合 splitChunks.cacheGroup 提取公共依赖
--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,10 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, '../src/main.jsx'),
+    // entry: path.resolve(__dirname, '../src/main.jsx'),
+    entry: {
+        main: path.resolve(__dirname, '../src/main.jsx'),
+        framework: ['react', 'react-dom'], // 配合 splitChunks cacheGroup 使用
+    },
     output: {
         path: path.resolve(__dirname, '../dist'),
         filename: '[name].[chunkhash:8].js',
+        chunkFilename: '[name].js', // 指定异步 chunk 的文件名, name 为 /* webapckChunkName: "xxx" */ 中的名字
         // publicPath: config[env].assetsPublicPath,
     },
     module: {
@@ -35,4 +40,25 @@ module.exports = {
             showFavicon: true,
         }),
     ],
+    optimization: {
+        splitChunks: {
+            maxInitialRequests: 4, // 最大并行请求的资源数 (首次加载时, 要求更高, 因此手动设置为更低)
+
+            // cacheGroup 自定义的分离规则 默认有 vendors 和 default
+            // 也可以添加新的规则，在里面对属性进行增加或修改。如果要禁用，则将其显示的设置为 false。
+            // 当一个模块同时符合多个 cacheGroup 时, 根据 priority 确定优先级
+            cacheGroups: {
+                framework: {
+                    // test: 匹配需要此缓存组的模块, 省略会选择所有模块
+                    test: 'framework',
+                    // test(module) {
+                    //     console.log('🚀 ~ module:', module.resource); // module 是每一个 模块文件, resource 是其路径
+                    //     return /node_modules/.test(modules.resource);
+                    // },
+                    name: 'framework',
+                    chunks: 'all',
+                },
+            },
+        },
+    },
 };
