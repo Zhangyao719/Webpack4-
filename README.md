e2c34b9eb9ae7c49d22f1dd0be2d0111a6a0597d
Zhangyao719 <1258858403@qq.com>
2023-04-12 15:25:49 +0800

    Set base config

---
+++ README.md
@@ -0,0 +1,3 @@
+# 从零配置 webpack4 + react 脚手架
+
+https://github.com/vortesnail/blog/issues/4
---
+++ build/webpack.base.conf.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/main.js'),
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].js',
+        // filename: '[name].[chunkhash].js',
+        // publicPath: config[env].assetsPublicPath,
+    },
+};
