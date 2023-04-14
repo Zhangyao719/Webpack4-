9c07fccdb83adc97f412ed6cf5026fd3dcaf1308
Zhangyao719 <1258858403@qq.com>
2023-04-14 10:49:08 +0800

    Optimize devServer HMR

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app.jsx';
 
+// 热更新优化 (不会刷新整个页面)
+// module https://v4.webpack.docschina.org/api/module-variables#module-hot-webpack-specific-
+// module.hot https://v4.webpack.docschina.org/api/hot-module-replacement
+if (module && module.hot) {
+    module.hot.accept();
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
